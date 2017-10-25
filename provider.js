1e3cfbc1f3bc9e1028b19473f29816a25ae414a5
Eric Huang <ele828@gmail.com>
2017-10-24 20:49:38 -0500

    Implement dependency injection feature (#422)
    * init module dependency injection (#406)
    
    * init module dependency injection
    
    * fix unit test
    
    * Implement DI features (#420)
    
    * implement dependency injection features
    
    * fix failed tests
    
    * Add unit tests & refactor code (#431)
    
    * update di and add tests
    
    * add unit test for DI
    
    * change registry key from class name to class reference && add unit tests
    
    * add tests and fix linting
    
    * switch to use assert if possible
    
    * resolve linting issue (#432)
    
    * Add DI document and fix lint (#437)
    
    * fix linting issue
    
    * add readme
    
    * code refactoring
    
    * update README
    
    * update dev-server
    
    * update README
    
    * fix typo
    
    * Add readme for DI & fix typo (#439)
    
    * merge code from master branch & fix typo
    
    * update readme

---
+++ provider.js
@@ -0,0 +1,51 @@
+export class Provider {
+  constructor(token, isPrivate) {
+    this.token = token;
+    // Provider instance reference
+    this.instance = null;
+    this.private = isPrivate || false;
+  }
+
+  setInstance(instance) {
+    this.instance = instance;
+  }
+
+  getInstance() {
+    return this.instance;
+  }
+
+  resolved() {
+    return this.instance !== null;
+  }
+}
+
+export class ClassProvider extends Provider {
+  constructor(token, klass, deps, isPrivate) {
+    super(token, isPrivate);
+    this.klass = klass;
+    this.deps = deps;
+  }
+}
+
+export class ExistingProvider extends Provider {
+  constructor(token, useExisting, isPrivate) {
+    super(token, isPrivate);
+    this.useExisting = useExisting || '';
+  }
+}
+
+export class FactoryProvider extends Provider {
+  constructor(token, func, deps, isPrivate) {
+    super(token, isPrivate);
+    this.func = func;
+    this.deps = deps || [];
+  }
+}
+
+export class ValueProvider extends Provider {
+  constructor(token, value, spread, isPrivate) {
+    super(token, isPrivate);
+    this.value = value;
+    this.spread = spread || false;
+  }
+}
