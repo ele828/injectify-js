f0427425a78ab12c8b502872b6c091bc4396683e
ele828 <ele828@gmail.com>
2018-08-08 16:19:36 +0800

    clean file structure

--- src/utils/is_type.js
+++ src/utils/is_type.js
@@ -9,7 +9,7 @@ export function isArray(x) {
 }
 
 export function isObject(x) {
-  return typeof x === 'object' && !isArray(x);
+  return Object.prototype.toString.call(x).slice(8, -1) === 'Object';
 }
 
 export function isFunction(x) {
